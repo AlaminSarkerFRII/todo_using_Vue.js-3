5c68ec9080d4d006466304e4f4fdf4827f52f103
alamin <alamin.education1997@gmail.com>
2022-11-26 00:10:28 +0600

    add styles

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { ref, onMounted, computed, watch } from 'vue'
+
+
+const todos = ref([])
+const name = ref('')
+const input_content = ref('')
+const input_category = ref(null)
+
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt;
+}))
+
+
+// watch
+
+watch(name, (newValue) => {
+  localStorage.setItem('name', newValue)
+})
+
+// get
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || '';
+})
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        whats up <input type="text" placeholder="type name" />
+      </h2>
+    </section>
 
-  <main>
-    <TheWelcome />
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>Whats on your todo list ?</h4>
+
+        <input type="text" placeholder="add your video" v-model="input_content">
+        
+        {{ input_content }}
+
+      </form>
+    </section>
+
   </main>
+
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
